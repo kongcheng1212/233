76257b6ccf1bbd66a12767e17301fa906bb8221d
kongcheng1212 <93602722+kongcheng1212@users.noreply.github.com>
2023-10-21 20:46:19 +0800

    Update kongcheng.py

--- kongcheng.py
+++ kongcheng.py
@@ -1,1 +1,276 @@
-11
+js_config:
+- Container:
+  - []
+  Env: M_WX_LUCK_DRAW_URL
+  KeyWord:
+  - - M_WX_LUCK_DRAW_URL
+  Name: 【M系】幸运抽奖
+  OverdueTime: 1800
+  Script: m_jd_wx_luckDraw.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Disable: 0
+  Env: M_WX_ADD_CART_URL
+  KeyWord:
+  - - M_WX_ADD_CART_URL
+  Name: 【M系】加购有礼
+  OverdueTime: 1800
+  Script: m_jd_wx_addCart.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Disable: 0
+  Env: M_WX_CENTER_DRAW_URL
+  KeyWord:
+  - - M_WX_CENTER_DRAW_URL
+  Name: 【M系】老虎机抽奖
+  OverdueTime: 1800
+  Script: m_jd_wx_centerDraw.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Disable: 0
+  Env: M_FAV_SHOP_ARGV
+  KeyWord:
+  - - M_FAV_SHOP_ARGV
+  Name: 【M系】收藏有礼
+  OverdueTime: 1800
+  Script: m_jd_fav_shop_gift.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Disable: 0
+  Env: M_FOLLOW_SHOP_ARGV
+  KeyWord:
+  - - M_FOLLOW_SHOP_ARGV
+  Name: 【M系】关注有礼
+  OverdueTime: 1800
+  Script: m_jd_follow_shop.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_SHOP_GIFT_URL
+  KeyWord:
+  - - M_WX_SHOP_GIFT_URL
+  Name: 【M系】关注有礼无线
+  OverdueTime: 1800
+  Script: m_jd_wx_shopGift.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_FOLLOW_DRAW_URL
+  KeyWord:
+  - - M_WX_FOLLOW_DRAW_URL
+  Name: 【M系】关注抽奖
+  OverdueTime: 1800
+  Script: m_jd_wx_followDraw.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_BUILD_DRAW_URL
+  KeyWord:
+  - - M_WX_BUILD_DRAW_URL
+  Name: 【M系】盖楼领奖
+  OverdueTime: 1800
+  Script: m_jd_wx_buildDraw.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_SECOND_DRAW_URL
+  KeyWord:
+  - - M_WX_SECOND_DRAW_URL
+  Name: 【M系列】读秒拼手速
+  Script: m_jd_wx_secondDraw.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_SHARE_URL
+  KeyWord:
+  - - M_WX_SHARE_URL
+  Name: 【Faker】M分享有礼
+  Script: m_jd_wx_share.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_TEAM_JF_URL
+  KeyWord:
+  - - M_WX_TEAM_JF_URL
+  Name: 【M系列】M组队瓜分积分
+  Script: m_jd_wx_team.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_TEAM_URL
+  KeyWord:
+  - - M_WX_TEAM_URL
+  Name: 【M系列】M组队瓜分京豆
+  Script: m_jd_wx_team.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_LEVEL_BIRTH_URL
+  KeyWord:
+  - - M_WX_LEVEL_BIRTH_URL
+  Name: 【M系列】M等级生日礼包
+  Script: m_jd_wx_levelBirth.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_JOY_INVITE_URL
+  KeyWord:
+  - - M_JOY_INVITE_URL
+  Name: 【M系列】M邀请有礼JOY
+  Script: m_jd_joy_invite.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_KNOWLEDGE_URL
+  KeyWord:
+  - - M_WX_KNOWLEDGE_URL
+  Name: 【M系列】知识超人
+  Script: m_jd_wx_knowledge.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_LOREAL_INVITE_URL
+  KeyWord:
+  - - M_LOREAL_INVITE_URL
+  Name: 【M系列】M邀请有礼LOREAL
+  Script: m_jd_loreal_invite.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_GYG_SHOP_ARGV
+  KeyWord:
+  - - M_GYG_SHOP_ARGV
+  Name: 【M系列】M店铺刮奖
+  Script: m_jd_shop_gyg.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_SHOP_POINT_EXCHANGE_ARGV
+  KeyWord:
+  - - M_SHOP_POINT_EXCHANGE_ARGV
+  Name: 【M系列】M店铺积分兑换
+  Script: m_jd_shop_pointExchange.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_COMPLETE_DRAW_URL
+  KeyWord:
+  - - M_WX_COMPLETE_DRAW_URL
+  Name: 【M系列】M完善有礼
+  Script: m_jd_wx_completeDraw.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_GAME_URL
+  KeyWord:
+  - - M_WX_GAME_URL
+  Name: 【M系列】M无线游戏
+  Script: m_jd_wx_game.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_UNPACK_DRAW_URL
+  KeyWord:
+  - - M_WX_UNPACK_DRAW_URL
+  Name: 【M系列】M分享福袋
+  Script: m_jd_wx_unPackDraw.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_DAILY_GIFT_URL
+  KeyWord:
+  - - M_WX_DAILY_GIFT_URL
+  Name: 【M系列】M每日领奖
+  Script: m_jd_wx_dailyGift.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_SHOP_SIGN_URL
+  KeyWord:
+  - - M_WX_SHOP_SIGN_URL
+  Name: 【M系列】M签到有礼
+  Script: m_jd_wx_shopSign.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_POINT_DRAW_URL
+  KeyWord:
+  - - M_WX_POINT_DRAW_URL
+  Name: 【M系列】M积分兑换
+  Script: m_jd_wx_pointDraw.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_CARTKOI_URL
+  KeyWord:
+  - - M_WX_CARTKOI_URL
+  Name: 【M系列】M购物车锦鲤
+  Script: m_jd_wx_cartKoi.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_FANS_DRAW_URL
+  KeyWord:
+  - - M_WX_FANS_DRAW_URL
+  Name: 【M系列】M粉丝互动
+  Script: m_jd_wx_fansDraw.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_OPEN_CARD_ARGV
+  KeyWord:
+  - - M_OPEN_CARD_ARGV
+  Name: 【M系列】M入会有礼
+  Script: m_jd_open_card.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_OPEN_CARD_FORCE_ARGV
+  KeyWord:
+  - - M_OPEN_CARD_FORCE_ARGV
+  Name: 【M系列】M强制入会
+  Script: m_jd_open_card_force.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
+  Env: M_WX_DADOUDOU_URL
+  KeyWord:
+  - - M_WX_DADOUDOU_URL
+  Name: 【M系列】M打豆豆
+  Script: m_jd_wx_dadoudou.js
+  TimeOut: 0
+  Wait: 2
+- Container:
+  - []
